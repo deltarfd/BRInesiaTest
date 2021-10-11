c065d87829cc90f7a070217e333f4990c2b9ec5a
deltarfd <deltarfd@live.com>
2021-10-11 22:15:08 +0700

    Initial Commit

---
+++ BRInesiaTest/BRInesiaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BRInesiaTestApp.swift
+//  BRInesiaTest
+//
+//  Created by Delta Rahmat Fajar Delviansyah on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BRInesiaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
